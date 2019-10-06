91354b4370c1f277d30f64c3ce92743b635baea9
Piernas <Piernas@users.noreply.github.com>
2019-10-06 21:56:44 +0200

    Update footer.php

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -10,7 +10,7 @@
   Released under the GNU General Public License
 */
 ?>
-
+<br>
 <footer class="row bg-dark text-light py-2 fixed-bottom">
   <div class="col-12 text-center">osCommerce Online Merchant Copyright &copy; 2000-<?php echo date('Y'); ?> <a href="https://www.oscommerce.com" target="_blank">osCommerce</a> (<a href="https://www.oscommerce.com/Us&amp;Legal" target="_blank">Copyright and Trademark Policy</a>)</div>
 </footer>
