488086b9e8ff3e1947fbd13a09bb88892935d709
Piernas <Piernas@users.noreply.github.com>
2019-10-06 22:58:11 +0200

    New languages page
    Introducing language ini files for easier installation

--- admin/languages.php
+++ admin/languages.php
@@ -13,196 +13,147 @@
   require('includes/application_top.php');
 
   $action = (isset($_GET['action']) ? $_GET['action'] : '');
+  $code = (isset($_GET['code']) ? $_GET['code'] : '');
+
+  require ('includes/classes/languages_setup.php');
 
   if (tep_not_null($action)) {
     switch ($action) {
-      case 'insert':
-        $name = tep_db_prepare_input($_POST['name']);
-        $code = tep_db_prepare_input(substr($_POST['code'], 0, 2));
-        $image = tep_db_prepare_input($_POST['image']);
-        $directory = tep_db_prepare_input($_POST['directory']);
-        $sort_order = (int)tep_db_prepare_input($_POST['sort_order']);
+/*
 
-        tep_db_query("insert into " . TABLE_LANGUAGES . " (name, code, image, directory, sort_order) values ('" . tep_db_input($name) . "', '" . tep_db_input($code) . "', '" . tep_db_input($image) . "', '" . tep_db_input($directory) . "', '" . tep_db_input($sort_order) . "')");
-        $insert_id = tep_db_insert_id();
+      case 'languages_install':
+        $languages__installer = new languages_install ('');
+        $languages__installer->install ($code);
+        $name =($languages__installer->installed[$code]['name']);
 
-// create additional categories_description records
-        $categories_query = tep_db_query("select c.categories_id, cd.categories_name from " . TABLE_CATEGORIES . " c left join " . TABLE_CATEGORIES_DESCRIPTION . " cd on c.categories_id = cd.categories_id where cd.language_id = '" . (int)$languages_id . "'");
-        while ($categories = tep_db_fetch_array($categories_query)) {
-          tep_db_query("insert into " . TABLE_CATEGORIES_DESCRIPTION . " (categories_id, language_id, categories_name) values ('" . (int)$categories['categories_id'] . "', '" . (int)$insert_id . "', '" . tep_db_input($categories['categories_name']) . "')");
-        }
+        $messageStack->add_session(sprintf(TEXT_INFO_LANGUAGE_INSTALLED, $name), 'success');
 
-// create additional products_description records
-        $products_query = tep_db_query("select p.products_id, pd.products_name, pd.products_description, pd.products_url from " . TABLE_PRODUCTS . " p left join " . TABLE_PRODUCTS_DESCRIPTION . " pd on p.products_id = pd.products_id where pd.language_id = '" . (int)$languages_id . "'");
-        while ($products = tep_db_fetch_array($products_query)) {
-          tep_db_query("insert into " . TABLE_PRODUCTS_DESCRIPTION . " (products_id, language_id, products_name, products_description, products_url) values ('" . (int)$products['products_id'] . "', '" . (int)$insert_id . "', '" . tep_db_input($products['products_name']) . "', '" . tep_db_input($products['products_description']) . "', '" . tep_db_input($products['products_url']) . "')");
-        }
-
-// create additional products_options records
-        $products_options_query = tep_db_query("select products_options_id, products_options_name from " . TABLE_PRODUCTS_OPTIONS . " where language_id = '" . (int)$languages_id . "'");
-        while ($products_options = tep_db_fetch_array($products_options_query)) {
-          tep_db_query("insert into " . TABLE_PRODUCTS_OPTIONS . " (products_options_id, language_id, products_options_name) values ('" . (int)$products_options['products_options_id'] . "', '" . (int)$insert_id . "', '" . tep_db_input($products_options['products_options_name']) . "')");
-        }
-
-// create additional products_options_values records
-        $products_options_values_query = tep_db_query("select products_options_values_id, products_options_values_name from " . TABLE_PRODUCTS_OPTIONS_VALUES . " where language_id = '" . (int)$languages_id . "'");
-        while ($products_options_values = tep_db_fetch_array($products_options_values_query)) {
-          tep_db_query("insert into " . TABLE_PRODUCTS_OPTIONS_VALUES . " (products_options_values_id, language_id, products_options_values_name) values ('" . (int)$products_options_values['products_options_values_id'] . "', '" . (int)$insert_id . "', '" . tep_db_input($products_options_values['products_options_values_name']) . "')");
-        }
-
-// create additional manufacturers_info records
-        $manufacturers_query = tep_db_query("select m.manufacturers_id, mi.manufacturers_url from " . TABLE_MANUFACTURERS . " m left join " . TABLE_MANUFACTURERS_INFO . " mi on m.manufacturers_id = mi.manufacturers_id where mi.languages_id = '" . (int)$languages_id . "'");
-        while ($manufacturers = tep_db_fetch_array($manufacturers_query)) {
-          tep_db_query("insert into " . TABLE_MANUFACTURERS_INFO . " (manufacturers_id, languages_id, manufacturers_url) values ('" . $manufacturers['manufacturers_id'] . "', '" . (int)$insert_id . "', '" . tep_db_input($manufacturers['manufacturers_url']) . "')");
-        }
-
-// create additional orders_status records
-        $orders_status_query = tep_db_query("select orders_status_id, orders_status_name from " . TABLE_ORDERS_STATUS . " where language_id = '" . (int)$languages_id . "'");
-        while ($orders_status = tep_db_fetch_array($orders_status_query)) {
-          tep_db_query("insert into " . TABLE_ORDERS_STATUS . " (orders_status_id, language_id, orders_status_name) values ('" . (int)$orders_status['orders_status_id'] . "', '" . (int)$insert_id . "', '" . tep_db_input($orders_status['orders_status_name']) . "')");
-        }
-
-        if (isset($_POST['default']) && ($_POST['default'] == 'on')) {
-          tep_db_query("update " . TABLE_CONFIGURATION . " set configuration_value = '" . tep_db_input($code) . "' where configuration_key = 'DEFAULT_LANGUAGE'");
-        }
-
-        tep_redirect(tep_href_link('languages.php', (isset($_GET['page']) ? 'page=' . $_GET['page'] . '&' : '') . 'lID=' . $insert_id));
+        tep_redirect(tep_href_link('languages.php', 'code=' . $code));
+      break;
+       
+      case 'languages_delete_confirm':
+        $languages__installer = new languages_install ('');
+        $languages__installer->uninstall ($code);
+        $name =($languages__installer->installed[$code]['name']);
+        
+        $messageStack->add_session(sprintf(TEXT_INFO_LANGUAGE_REMOVED, $name ), 'success');
+        tep_redirect(tep_href_link('languages.php'));
         break;
-      case 'save':
-        $lID = tep_db_prepare_input($_GET['lID']);
-        $name = tep_db_prepare_input($_POST['name']);
-        $code = tep_db_prepare_input(substr($_POST['code'], 0, 2));
-        $image = tep_db_prepare_input($_POST['image']);
-        $directory = tep_db_prepare_input($_POST['directory']);
-        $sort_order = (int)tep_db_prepare_input($_POST['sort_order']);
-
-        tep_db_query("update " . TABLE_LANGUAGES . " set name = '" . tep_db_input($name) . "', code = '" . tep_db_input($code) . "', image = '" . tep_db_input($image) . "', directory = '" . tep_db_input($directory) . "', sort_order = '" . tep_db_input($sort_order) . "' where languages_id = '" . (int)$lID . "'");
-
-        if ($_POST['default'] == 'on') {
-          tep_db_query("update " . TABLE_CONFIGURATION . " set configuration_value = '" . tep_db_input($code) . "' where configuration_key = 'DEFAULT_LANGUAGE'");
-        }
-
-        tep_redirect(tep_href_link('languages.php', 'page=' . $_GET['page'] . '&lID=' . $_GET['lID']));
-        break;
-      case 'deleteconfirm':
-        $lID = tep_db_prepare_input($_GET['lID']);
-
-        $lng_query = tep_db_query("select languages_id from " . TABLE_LANGUAGES . " where code = '" . DEFAULT_CURRENCY . "'");
+      case 'languages_delete':
+        $lng_query = tep_db_query("select code from " . TABLE_LANGUAGES . " where languages_id = '" . $code . "'");
         $lng = tep_db_fetch_array($lng_query);
-        if ($lng['languages_id'] == $lID) {
-          tep_db_query("update " . TABLE_CONFIGURATION . " set configuration_value = '' where configuration_key = 'DEFAULT_CURRENCY'");
-        }
-
-        tep_db_query("delete from " . TABLE_CATEGORIES_DESCRIPTION . " where language_id = '" . (int)$lID . "'");
-        tep_db_query("delete from " . TABLE_PRODUCTS_DESCRIPTION . " where language_id = '" . (int)$lID . "'");
-        tep_db_query("delete from " . TABLE_PRODUCTS_OPTIONS . " where language_id = '" . (int)$lID . "'");
-        tep_db_query("delete from " . TABLE_PRODUCTS_OPTIONS_VALUES . " where language_id = '" . (int)$lID . "'");
-        tep_db_query("delete from " . TABLE_MANUFACTURERS_INFO . " where languages_id = '" . (int)$lID . "'");
-        tep_db_query("delete from " . TABLE_ORDERS_STATUS . " where language_id = '" . (int)$lID . "'");
-        tep_db_query("delete from " . TABLE_LANGUAGES . " where languages_id = '" . (int)$lID . "'");
-
-        tep_redirect(tep_href_link('languages.php', 'page=' . $_GET['page']));
+        echo '<span id="title"><strong><i class="fas fa-trash fa-lg"></i> ' . TEXT_INFO_HEADING_DELETE_LANGUAGE . '</strong></span>';
+        echo '<span id="content">' .TEXT_INFO_DELETE_INTRO . PHP_EOL;
+        echo '</span>';
+        
+        exit;
+        
+        break;*/
+/*
+      case 'languages_make_default':
+        $code = tep_db_prepare_input($_GET['code']);
+        tep_db_query("update " . TABLE_CONFIGURATION . " set configuration_value = '" . tep_db_input($code) . "' where configuration_key = 'DEFAULT_LANGUAGE'");
+        tep_redirect(tep_href_link('languages.php', 'code=' . $code));
         break;
-      case 'delete':
-        $lID = tep_db_prepare_input($_GET['lID']);
-
-        $lng_query = tep_db_query("select code from " . TABLE_LANGUAGES . " where languages_id = '" . (int)$lID . "'");
-        $lng = tep_db_fetch_array($lng_query);
-
-        $remove_language = true;
-        if ($lng['code'] == DEFAULT_LANGUAGE) {
-          $remove_language = false;
-          $messageStack->add(ERROR_REMOVE_DEFAULT_LANGUAGE, 'error');
-        }
-        break;
+*/
     }
+
   }
 
   require('includes/template_top.php');
-?>
+  require ('includes/classes/modules_pages.php');
 
-    <table border="0" width="100%" cellspacing="0" cellpadding="2">
-      <tr>
-        <td width="100%"><table border="0" width="100%" cellspacing="0" cellpadding="0">
-          <tr>
-            <td class="pageHeading"><?php echo HEADING_TITLE; ?></td>
-            <td class="pageHeading" align="right"><?php echo tep_draw_separator('pixel_trans.gif', HEADING_IMAGE_WIDTH, HEADING_IMAGE_HEIGHT); ?></td>
-          </tr>
-        </table></td>
-      </tr>
-      <tr>
-        <td><table border="0" width="100%" cellspacing="0" cellpadding="0">
-          <tr>
-            <td valign="top"><table border="0" width="100%" cellspacing="0" cellpadding="2">
-              <tr class="dataTableHeadingRow">
-                <td class="dataTableHeadingContent"><?php echo TABLE_HEADING_LANGUAGE_NAME; ?></td>
-                <td class="dataTableHeadingContent"><?php echo TABLE_HEADING_LANGUAGE_CODE; ?></td>
-                <td class="dataTableHeadingContent" align="right"><?php echo TABLE_HEADING_ACTION; ?>&nbsp;</td>
-              </tr>
+  $languages_modules_installed = new modules_pages ('languages_installed');
+  $oscTemplate->addBlock('<script>' . $languages_modules_installed->get_javascript() . '</script>', 'admin_footer_scripts');
+
+?>
+  <div class="d-flex justify-content-between">
+    <div class="mr-auto p-2 pageHeading"><i class="fas fa-language fa-lg"></i> <?= HEADING_TITLE ?></div>
+
+    <div class="py-2">
+      <a href="<?= tep_href_link('modules_pages.php?page=customers&desired_groups=' . urlencode (json_encode(array(array('languages'), array('languages_installed', 'languages_uninstalled'))))) ?>" class="btn btn-info btn-sm"><i class="fas fa-cog"></i></a>
+
+    </div>
+  </div>
+
+  <table class="table table-sm" id="installed-languages-table">
+    <thead>
+    <tr class="table-info">
+      <?= $languages_modules_installed->get_table_header(); ?>
+      <th class="actions"><?php echo TABLE_HEADING_ACTION; ?>&nbsp;</th>
+
+    </tr>
+    </thead>
+    <tbody>
 <?php
-  $languages_query_raw = "select languages_id, name, code, image, directory, sort_order from " . TABLE_LANGUAGES . " order by sort_order";
-  $languages_split = new splitPageResults($_GET['page'], MAX_DISPLAY_SEARCH_RESULTS, $languages_query_raw, $languages_query_numrows);
+  $languages_query_raw = "select languages_id, code from " . TABLE_LANGUAGES . " order by sort_order";
   $languages_query = tep_db_query($languages_query_raw);
 
+
   while ($languages = tep_db_fetch_array($languages_query)) {
-    if ((!isset($_GET['lID']) || (isset($_GET['lID']) && ($_GET['lID'] == $languages['languages_id']))) && !isset($lInfo) && (substr($action, 0, 3) != 'new')) {
-      $lInfo = new objectInfo($languages);
-    }
+//    echo $languages['code'];
+      $languages_setup = new languages_setup ($languages['code']);
 
-    if (isset($lInfo) && is_object($lInfo) && ($languages['languages_id'] == $lInfo->languages_id) ) {
-      echo '                  <tr id="defaultSelected" class="dataTableRowSelected" onmouseover="rowOverEffect(this)" onmouseout="rowOutEffect(this)" onclick="document.location.href=\'' . tep_href_link('languages.php', 'page=' . $_GET['page'] . '&lID=' . $lInfo->languages_id . '&action=edit') . '\'">' . "\n";
-    } else {
-      echo '                  <tr class="dataTableRow" onmouseover="rowOverEffect(this)" onmouseout="rowOutEffect(this)" onclick="document.location.href=\'' . tep_href_link('languages.php', 'page=' . $_GET['page'] . '&lID=' . $languages['languages_id']) . '\'">' . "\n";
-    }
+      if (isset($code) && ($languages['code'] == $code) ) {
 
-    if (DEFAULT_LANGUAGE == $languages['code']) {
-      echo '                <td class="dataTableContent"><strong>' . $languages['name'] . ' (' . TEXT_DEFAULT . ')</strong></td>' . "\n";
-    } else {
-      echo '                <td class="dataTableContent">' . $languages['name'] . '</td>' . "\n";
-    }
+  ?>
+              <tr class="clickable table-success">
+<?php
+      } else {
+  ?>
+              <tr class="clickable">
+<?php
+      }
 ?>
-                <td class="dataTableContent"><?php echo $languages['code']; ?></td>
-                <td class="dataTableContent" align="right"><?php if (isset($lInfo) && is_object($lInfo) && ($languages['languages_id'] == $lInfo->languages_id)) { echo tep_image('images/icon_arrow_right.gif'); } else { echo '<a href="' . tep_href_link('languages.php', 'page=' . $_GET['page'] . '&lID=' . $languages['languages_id']) . '">' . tep_image('images/icon_info.gif', IMAGE_ICON_INFO) . '</a>'; } ?>&nbsp;</td>
-              </tr>
+            <?= $languages_modules_installed->get_table_row($languages['code']); ?>
+      <td class="actions">
+        <?= $languages_modules_installed->get_action_buttons($languages['code']); ?>
+      </td>
+    </tr>
 <?php
   }
 ?>
-              <tr>
-                <td colspan="3"><table border="0" width="100%" cellspacing="0" cellpadding="2">
-                  <tr>
-                    <td class="smallText" valign="top"><?php echo $languages_split->display_count($languages_query_numrows, MAX_DISPLAY_SEARCH_RESULTS, $_GET['page'], TEXT_DISPLAY_NUMBER_OF_LANGUAGES); ?></td>
-                    <td class="smallText" align="right"><?php echo $languages_split->display_links($languages_query_numrows, MAX_DISPLAY_SEARCH_RESULTS, MAX_DISPLAY_PAGE_LINKS, $_GET['page']); ?></td>
-                  </tr>
+    </tbody>
+  </table>
 <?php
-  if (empty($action)) {
+  $languages_modules_uninstalled = new modules_pages ('languages_uninstalled');
+
+  $uninstalled_array = $languages_setup->uninstalled;
+  if (count((array)$uninstalled_array) > 0){
 ?>
-                  <tr>
-                    <td class="smallText" align="right" colspan="2"><?php echo tep_draw_button(IMAGE_NEW_LANGUAGE, 'plus', tep_href_link('languages.php', 'page=' . $_GET['page'] . '&lID=' . $lInfo->languages_id . '&action=new')); ?></td>
-                  </tr>
+  <table class="table table-sm">
+    <thead>
+    <tr class="table-info">
+      <?= $languages_modules_uninstalled->get_table_header(); ?>
+      <th class="actions"><?php echo TABLE_HEADING_ACTION; ?>&nbsp;</th>
+    </tr>
+    </thead>
+    <tbody>
+<?php
+    foreach ($uninstalled_array as $key => $value) {
+?>
+      <tr class="clickable">
+        <?= $languages_modules_uninstalled->get_table_row($key); ?>
+      <td class="actions">
+        <?= $languages_modules_uninstalled->get_action_buttons($key); ?>
+      </td>
+      </tr>
+
+      
+      
 <?php
   }
 ?>
-                </table></td>
-              </tr>
-            </table></td>
+    </tbody>
+  </table>
+  
 <?php
+    }
+/*
   $heading = array();
   $contents = array();
 
   switch ($action) {
-    case 'new':
-      $heading[] = array('text' => '<strong>' . TEXT_INFO_HEADING_NEW_LANGUAGE . '</strong>');
-
-      $contents = array('form' => tep_draw_form('languages', 'languages.php', 'action=insert'));
-      $contents[] = array('text' => TEXT_INFO_INSERT_INTRO);
-      $contents[] = array('text' => '<br />' . TEXT_INFO_LANGUAGE_NAME . '<br />' . tep_draw_input_field('name'));
-      $contents[] = array('text' => '<br />' . TEXT_INFO_LANGUAGE_CODE . '<br />' . tep_draw_input_field('code'));
-      $contents[] = array('text' => '<br />' . TEXT_INFO_LANGUAGE_IMAGE . '<br />' . tep_draw_input_field('image', 'icon.gif'));
-      $contents[] = array('text' => '<br />' . TEXT_INFO_LANGUAGE_DIRECTORY . '<br />' . tep_draw_input_field('directory'));
-      $contents[] = array('text' => '<br />' . TEXT_INFO_LANGUAGE_SORT_ORDER . '<br />' . tep_draw_input_field('sort_order'));
-      $contents[] = array('text' => '<br />' . tep_draw_checkbox_field('default') . ' ' . TEXT_SET_DEFAULT);
-      $contents[] = array('align' => 'center', 'text' => '<br />' . tep_draw_button(IMAGE_SAVE, 'disk', null, 'primary') . tep_draw_button(IMAGE_CANCEL, 'close', tep_href_link('languages.php', 'page=' . $_GET['page'] . '&lID=' . $_GET['lID'])));
-      break;
     case 'edit':
       $heading[] = array('text' => '<strong>' . TEXT_INFO_HEADING_EDIT_LANGUAGE . '</strong>');
 
@@ -252,6 +203,15 @@
     </table>
 
 <?php
+*/
+
+  require ("includes/classes/modal.php");
+  
+  $modal = new modal();
+  $modal->button_delete = true;
+  $modal->button_cancel = true;
+  $modal->output();
+
   require('includes/template_bottom.php');
   require('includes/application_bottom.php');
 ?>
