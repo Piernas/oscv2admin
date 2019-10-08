d2f0a13e5383d3bd1336b579e51aa72a60423f26
Piernas <Piernas@users.noreply.github.com>
2019-10-08 22:22:02 +0200

    Remove dashboard modules

--- admin/index.php
+++ admin/index.php
@@ -19,46 +19,47 @@
 <?php
   if ( defined('MODULE_ADMIN_DASHBOARD_INSTALLED') && tep_not_null(MODULE_ADMIN_DASHBOARD_INSTALLED) ) {
     $adm_array = explode(';', MODULE_ADMIN_DASHBOARD_INSTALLED);
+  IF (COUNT($adm_array)>0) {
+      $col = 0;
 
-    $col = 0;
+      for ( $i=0, $n=sizeof($adm_array); $i<$n; $i++ ) {
+        $adm = $adm_array[$i];
 
-    for ( $i=0, $n=sizeof($adm_array); $i<$n; $i++ ) {
-      $adm = $adm_array[$i];
+        $class = substr($adm, 0, strrpos($adm, '.'));
 
-      $class = substr($adm, 0, strrpos($adm, '.'));
-
-      if ( !class_exists($class) ) {
-        include('includes/languages/' . $language . '/modules/dashboard/' . $adm);
-        include('includes/modules/dashboard/' . $class . '.php');
-      }
-
-      $ad = new $class();
-
-      if ( $ad->isEnabled() ) {
-        if ($col < 1) {
-          echo '          <tr>' . "\n";
+        if ( !class_exists($class) ) {
+          include('includes/languages/' . $language . '/modules/dashboard/' . $adm);
+          include('includes/modules/dashboard/' . $class . '.php');
         }
 
-        $col++;
+        $ad = new $class();
 
-        if ($col <= 2) {
-          echo '            <td width="50%" valign="top">' . "\n";
-        }
-
-        echo $ad->getOutput();
-
-        if ($col <= 2) {
-          echo '            </td>' . "\n";
-        }
-
-        if ( !isset($adm_array[$i+1]) || ($col == 2) ) {
-          if ( !isset($adm_array[$i+1]) && ($col == 1) ) {
-            echo '            <td width="50%" valign="top">&nbsp;</td>' . "\n";
+        if ( $ad->isEnabled() ) {
+          if ($col < 1) {
+            echo '          <tr>' . "\n";
           }
 
-          $col = 0;
+          $col++;
 
-          echo '  </tr>' . "\n";
+          if ($col <= 2) {
+            echo '            <td width="50%" valign="top">' . "\n";
+          }
+
+          echo $ad->getOutput();
+
+          if ($col <= 2) {
+            echo '            </td>' . "\n";
+          }
+
+          if ( !isset($adm_array[$i+1]) || ($col == 2) ) {
+            if ( !isset($adm_array[$i+1]) && ($col == 1) ) {
+              echo '            <td width="50%" valign="top">&nbsp;</td>' . "\n";
+            }
+
+            $col = 0;
+
+            echo '  </tr>' . "\n";
+          }
         }
       }
     }
