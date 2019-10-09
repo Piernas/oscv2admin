dcaab867456e451906cf6d7e7b959c0e8d823615
Piernas <Piernas@users.noreply.github.com>
2019-10-09 15:07:35 +0200

    tax zones revamp (formerly geo-zones)

---
+++ admin/includes/languages/english/tax_zones.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ *
+ * tax_zones.php
+ *
+ * English language file for tax_zones.php
+ *
+ * Copyright 2019 Juan Manuel de Castro
+ * Released under the GPL v3.0 License
+ *
+ *
+ *
+ */
+ 
+  define('HEADING_TITLE','Tax zones');
+  define('HEADING_TITLE_SEARCH','Search');
