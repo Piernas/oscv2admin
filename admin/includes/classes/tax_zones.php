880a8f419c10fa01ac36fb623ea417046fd3ca2e
Piernas <Piernas@users.noreply.github.com>
2019-10-10 01:41:11 +0200

    tax-zones

--- admin/includes/classes/tax_zones.php
+++ admin/includes/classes/tax_zones.php
@@ -11,39 +11,90 @@
  *
  *
  */
- 
+
   class tax_zones {
-    
+
     function __construct () {
       $this->get_tax_zones();
+      $this->get_zone_names();
     }
-    
+
     function get_tax_zones () {
       $zones_query = tep_db_query("
-      select gz.geo_zone_id, gz.geo_zone_name, 
-      a.zone_country_id, c.countries_name, c.countries_iso_code_2,
-      a.zone_id, z.zone_name, 
-      a.association_id 
-      from zones_to_geo_zones a left join countries c on a.zone_country_id = c.countries_id left join zones z on a.zone_id = z.zone_id right join geo_zones gz on gz.geo_zone_id = a.geo_zone_id ");
-      
+        SELECT gz.geo_zone_id, gz.geo_zone_name, gz.geo_zone_description,
+        a.zone_country_id, c.countries_name, c.countries_iso_code_2,
+        a.zone_id, z.zone_name,
+        a.association_id
+        FROM zones_to_geo_zones a
+        LEFT JOIN countries c on a.zone_country_id = c.countries_id
+        LEFT JOIN zones z on a.zone_id = z.zone_id
+        RIGHT JOIN geo_zones gz
+        ON gz.geo_zone_id = a.geo_zone_id ");
+
       while ($zones = tep_db_fetch_array($zones_query)) {
 
         $this->tax_zones [$zones['geo_zone_id']]['geo_zone_name'] =  $zones['geo_zone_name'];
+        $this->tax_zones [$zones['geo_zone_id']]['geo_zone_description'] =  $zones['geo_zone_description'];
         if ($zones['zone_country_id']) {
+
           $this->tax_zones [$zones['geo_zone_id']]['geo_zone_countries'][$zones['zone_country_id']]['country_name'] =   $zones['countries_name'];
+
           $this->tax_zones [$zones['geo_zone_id']]['geo_zone_countries'][$zones['zone_country_id']]['country_iso_code_2'] =   $zones['countries_iso_code_2'];
+
+          if (!$zones['zone_name']) $zones['zone_name'] = TEXT_ALL_ZONES;
+
+          $this->tax_zones [$zones['geo_zone_id']]['geo_zone_countries'][$zones['zone_country_id']]['country_zones'][$zones['zone_id']] =  array('zone_name' => $zones['zone_name'], 'association_id' => $zones['association_id']);
+
         } else {
           $this->tax_zones [$zones['geo_zone_id']]['geo_zone_countries'] = array();
         }
-          
-          
-          
-         ;
-        if ($zones['zone_country_id']) {
-          if (!$zones['zone_name']) $zones['zone_name'] = TEXT_ALL_ZONES;
-          $this->tax_zones [$zones['geo_zone_id']]['geo_zone_countries'][$zones['zone_country_id']]['country_zones'][$zones['zone_id']] =  array('zone_name' => $zones['zone_name'], 'association_id' => $zones['association_id']);
-        };
+
       }
       return $this->tax_zones;
+    }
+
+    function get_zone_names () {
+      $zone_names_query = tep_db_query ('SELECT zone_id, zone_name FROM zones ORDER BY zone_id');
+      $this->zone_names_array[0] = TEXT_ALL_ZONES;
+      while ($zone_names = tep_db_fetch_array($zone_names_query)) {
+        $this->zone_names_array[$zone_names['zone_id']] = $zone_names['zone_name'];
+      }
+    }
+
+
+    function get_remaining_zones ($country_id, $tax_zone_id) {
+      $returned = array();
+
+      $not_present_query = tep_db_query ('SELECT zone_id, zone_country_id
+              FROM
+               (
+                 SELECT zones_to_geo_zones.zone_id, zones_to_geo_zones.zone_country_id
+                 FROM zones_to_geo_zones
+                   WHERE zones_to_geo_zones.geo_zone_id = ' . (int)$tax_zone_id . '
+                   AND zone_country_id = ' . (int)$country_id . '
+                 UNION ALL
+                 SELECT zones.zone_id, zones.zone_country_id
+                   FROM zones
+                      WHERE zones.zone_country_id = ' . (int)$country_id . '
+              )  t
+              GROUP BY zone_id, zone_country_id
+              HAVING COUNT(*) = 1
+              ORDER BY zone_id');
+
+      while($zones = tep_db_fetch_array($not_present_query)){
+        if ($zones['zone_id'] != 0) {
+          $returned [$zones['zone_id']] = $this->zone_names_array [$zones['zone_id']];
+        }
+      }
+
+      return $returned;
+    }
+
+    function remove_zone ($association_id) {
+      tep_db_query("delete from zones_to_geo_zones where association_id = '" . (int)$association_id . "'");
+    }
+
+    function zones_to_add () {
+
     }
   }
--- admin/tax_zones.php
+++ admin/tax_zones.php
@@ -15,15 +15,36 @@
 
   require('includes/application_top.php');
 
-
   require('includes/classes/tax_zones.php');
-
-  require('includes/template_top.php');
-
-
   $tax_zones_class = new tax_zones ();
   $tax_zones_array = $tax_zones_class->tax_zones;
 
+  $action = (isset($_GET['action']) ? $_GET['action'] : '');
+
+  if (tep_not_null($action)) {
+    switch ($action) {
+
+     case 'zone_delete':
+        $association_id = (int)$_GET['association_id'];
+        echo '<span id="title"><strong><i class="fas fa-trash fa-lg"></i> ' . TEXT_INFO_HEADING_DELETE_ZONE . '</strong></span>';
+        echo '<span id="content">' . PHP_EOL;
+        echo '<p>' . TEXT_INFO_DELETE_ZONE_INTRO . '</p>' . PHP_EOL;
+        echo '</span>';
+        exit();
+        break;
+
+      case 'zone_delete_confirm':
+        $association_id = (int)$_GET['association_id'];
+//        $countres_array = $tax_zones_class->get_countries_list ();
+//        $country_name = $countres_array[$country_id];
+        $tax_zones_class->remove_zone ($association_id);
+        $messageStack->add_session(sprintf (MESSAGE_ZONE_DELETED),'success');
+        tep_redirect(tep_href_link('tax_zones.php'));
+        break;
+    }
+  }
+
+  require('includes/template_top.php');
 
 ?>
 
@@ -32,7 +53,7 @@
       <div class="d-flex justify-content-between">
         <div class="mr-auto pageHeading"><i class="fas fa-users fa-lg"></i> <?= HEADING_TITLE ?></div>
           <div class="pr-2">
-            <?= tep_draw_button('', 'fas fa-plus-circle', null, null, array('type' => 'button', 'params' => 'onclick="javascript:Modal();"'), 'btn-info btn-sm') ?>
+            <?= tep_draw_button(TEXT_BUTTON_NEW_TAX_ZONE, 'fas fa-plus-circle', null, null, array('type' => 'button', 'params' => 'onclick="javascript:Modal();"'), 'btn-info btn-sm') ?>
           </div>
           <div>
             <?= tep_draw_form('search', basename($PHP_SELF),"" ,"get" ,'class="form-inline"') ?>
@@ -54,11 +75,18 @@
   $active=' active';
 
   foreach ($tax_zones_array as $tax_zone_id => $tax_zones ) {
+
+    if (!$tax_zones_array[$tax_zone_id]['geo_zone_countries']) {
+      $warning ='<span class="fas fa-exclamation-triangle text-warning pr-2"></span>';
+    } else {
+      $warning ="";
+    }
+
+
 ?>
-            <li class="nav-item"><a class="nav-link<?= $active ?>" data-target ="#section_<?= $tax_zone_id ?>" data-toggle="tab"><?= $tax_zones['geo_zone_name'] ?></a></li>
+            <li class="nav-item"><a class="nav-link<?= $active ?>" data-target ="#section_<?= $tax_zone_id?>" data-toggle="tab"><?= $warning ?><?= $tax_zones['geo_zone_name'] ?></a></li>
 <?php
     $active="";
-/////////    print_r ($tax_zones_array[$tax_zone_id]['geo_zone_countries']);
   }
 ?>
           </ul>
@@ -74,6 +102,17 @@
 ?>
 
         <div id="section_<?= $tax_zone_id ?>" class="tab-pane <?= $active ?>">
+        
+          <div class="d-flex justify-content-between">
+          <div><h4 class="pr-3 align-middle"><?= $tax_zones['geo_zone_description'] ?></h4></div>
+          <div>
+          <button class="btn btn-success" type="button">Add* <i class="fas fa-plus-circle"></i></a></button>
+          <button class="btn btn-info" type="button">Edit* <i class="fas fa-edit"></i></a></button>
+
+          <button class="btn btn-danger" type="button">remove* <i class="fas fa-trash"></i></a></button>
+          <?= tep_draw_button(IMAGE_REMOVE, 'fas fa-minus-circle fa-lg', null, null, array('type' => 'button', 'params' => 'onclick="javascript:ModalAddCountry();"'), 'btn-danger') ?> 
+          </div>
+          </div>
 <?php
 
       if (!$tax_zones_array[$tax_zone_id]['geo_zone_countries']) {
@@ -85,10 +124,19 @@
     foreach ($tax_zones_array[$tax_zone_id]['geo_zone_countries'] as $country_key => $country_values) {
 
 ?>
-  <h3><span class="flag-icon flag-icon-<?= strtolower( $country_values['country_iso_code_2']) ?> mr-3"></span><?= $country_values['country_name'] ?></h3>
+
+          <hr>
+          <div class="d-flex justify-content-between h5">
+
+            <div><span class="flag-icon flag-icon-<?= strtolower( $country_values['country_iso_code_2']) ?> mr-3"></span><?= $country_values['country_name'] ?></div>
+            <div>
+            <?= tep_draw_button(IMAGE_REMOVE, 'fas fa-plus-circle fa-lg', null, null, array('type' => 'button', 'params' => 'onclick="javascript:ModalAddCountry();"'), 'btn-info btn-danger') ?>
+            </div>
+          </div>
           <table class="table table-sm">
             <thead>
               <tr class="table-info">
+              <td><?= TABLE_HEADING_ID ?></td>
               <td><?= TABLE_HEADING_ZONE ?></td>
               <td class="actions"><?= TABLE_HEADING_ACTION ?></td>
               </tr>
@@ -96,21 +144,43 @@
           <tbody>
 <?php
 
-
-
     foreach ($tax_zones_array[$tax_zone_id]['geo_zone_countries'][$country_key]['country_zones'] as $zone_key => $zone_values) {
 
+// print_r ($zone_values);
+
 ?>
-            <tr>
+            <tr id="tax_zone_<?= $zone_values['association_id'] ?>">
+              <td><?= $zone_key ?></td>
               <td><?= $zone_values['zone_name'] ?></td>
-              <td class="actions"><a href="javascript:ModalDeleteCountry(<?= $country['countries_id'] ?>);"><i class="fas fa-trash fa-lg text-danger"></i></a></td>
+              <td class="actions"><a href="javascript:ModalDeleteTaxZone(<?= $zone_values['association_id'] ?>);"><i class="fas fa-trash fa-lg text-danger"></i></a></td>
 
             </tr>
-          
-          
-<?php
-    }
 
+
+<?php
+
+    }
+?>
+            <tr class="table-info">
+              <td></td>
+
+              <td class="text-right">*Select more zones from this country</td>
+              <td class="actions"><i class="fas fa-caret-square-down fa-lg text-primary"></i></a></td>
+
+            </tr>
+<?php
+    $remaining_zones = $tax_zones_class->get_remaining_zones($country_key,$tax_zone_id);
+
+    foreach ($remaining_zones as $zone_key => $zone_name) {
+?>
+
+            <tr class="table-warning unused-zone">
+              <td><?= $zone_key ?></td>
+              <td><?= $zone_name ?></td>
+              <td class="actions"><a href="javascript:ModalAddZone(<?= $zone_values['association_id'] ?>);"><i class="fas fa-plus-circle fa-lg text-info"></i></a></td>
+
+            </tr><?php
+    }
 ?>
           </tbody>
           </table>
@@ -142,8 +212,46 @@
   $stylesheet = '<link rel="stylesheet" href="' . tep_catalog_href_link ( 'ext/flag-icon-css/css/flag-icon.min.css') . '">' . PHP_EOL;
   $oscTemplate->addBlock($stylesheet, 'admin_footer_scripts');
 
+  // Loads modal:
+  require ("includes/classes/modal.php");
+
+  $modal = new modal();
+  $modal->button_save = true;
+  $modal->button_delete = true;
+  $modal->button_cancel = true;
+  $modal->output();
+?>
+<script>
+function ModalDeleteTaxZone(association_id){
+  // Formulario
+  $("form > .modal-content").unwrap();
+  $(".modal-content").wrap('<form id="tax_zones" name="tax_zones" action="tax_zones.php?action=zone_delete_confirm&association_id=' + association_id + '" method="post">')
+
+  // Botones
+  $("#ModalButtonCancel").show();
+  $("#ModalButtonSubmit").hide();
+  $("#ModalButtonSave").hide();
+  $("#ModalButtonDelete").show();
 
 
+  var params = {"association_id" : association_id, "action" : "zone_delete"};
+  $.ajax({
+    data:  params,
+    url:   'tax_zones.php',
+    type:  'get',
+    cache: false,
+    beforeSend: function () {
+      $(".modal-body").html('<div class="text-center"><i class="fas fa-spinner fa-pulse fa-3x fa-fw text-info"></i><span class="sr-only">Loading...</span></div>');
+    },
+    success:  function (response) {
+      $(".modal-title").html($(response).filter('#title').html());
+      $(".modal-body").html($(response).filter('#content').html());
+      $("#tax_zonesModal").modal('show');
+    }
+  });
+}
+</script>
+<?php
   require('includes/template_bottom.php');
 
   require('includes/application_bottom.php');
