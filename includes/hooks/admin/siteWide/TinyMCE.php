24b765e16479869cb3dede507b02cf4c60dd0f95
Piernas <Piernas@users.noreply.github.com>
2019-11-01 09:45:55 +0100

    Update TinyMCE.php
    changed to local instead of cdn

--- includes/hooks/admin/siteWide/TinyMCE.php
+++ includes/hooks/admin/siteWide/TinyMCE.php
@@ -6,8 +6,8 @@
     
     function listen_injectSiteEnd() {
       $this->siteend = '<!-- tinymce hooked -->' . PHP_EOL;
-//      $this->siteend .= '<script src="' . tep_catalog_href_link ('ext/tinymce/tinymce.min.js') . '"></script>' . PHP_EOL;
-      $this->siteend .= '<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>';
+      $this->siteend .= '<script src="' . tep_catalog_href_link ('ext/tinymce/tinymce.min.js') . '"></script>' . PHP_EOL;
+//      $this->siteend .= '<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>';
       return $this->siteend;
    } 
   }
